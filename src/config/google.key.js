2a89db5741aa7b69b4e45f4f1f45d5fb03fa124f
seongeun <seongeun.cho.dev@gmail.com>
2024-02-19 12:08:27 +0900

    secret

--- src/config/google.key.js
+++ src/config/google.key.js
@@ -1,12 +1,3 @@
 module.exports = {
-  "type": "service_account",
-  "project_id": "minimum-hexscan",
-  "private_key_id": "ba488fe9550d56722bf23e32a65a6604ffbc40c1",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCY2xr9zXTMECuk\nqS9SMsk2ctb7EHjiOD1G5eDFQqRX0qH3BzwoWietX7a5LyFniUfBilcPFLOJxR5r\nep6agyRr4ttOoQcd4J/bnbDgUrvEYSnzle04XMRB1L54LH1hgrHNIvt6cAVgskmz\nfNgVw2M7JMSEb1LGmewYZKqBYScYbRY8ZSaSxwSxxdD4Y4l6ag4+XUhJo4PdOTxP\nvBfEtp7JdTXOo7tpYJQ+c6ZDxpYZXmmMQQhiL0tLBdNGWMMGyR9t7BqdN6koxP61\nMx/iA/CoSG7lXzXadgBRMkv9TcSD29yTuBEgFPV+Up/7x2P98DadPnRw0mtJOUW0\nG7uM3b8tAgMBAAECggEAGAjNme4yCPsa7alnhihzoHLWsEPjCgj3Q5xWJxYnMqLt\nMiMqV5rzZayo1b3eJgI9LOakcTvbThSXR6WQZjGsKqWRP7AMpytAX38QRnljyklP\n2BvxIJ1fg41gxJMURN6Qt/Vh7trJ0a4+7fC5plRiWDScDMnXTWB68cv0TmMZC1sv\nW7nYk6GKZ14PB9uSEr6ewT1rCo/ybyXhmOlMmiZB6l6RON+RdCFTQPR4rzcvbZ+8\nk4CsB+z4eLVds5+q0x5WIRTO6i9dysqZ4dSD7AQyKD5/B6lMiFtM47ol9oIcOqXK\naCaSO1wps/ZxxX6cnCdkmbwfgWdux+DvFWWTcqFC4QKBgQDTrs49i8YsbriGruWh\nO3hRoJTvhHzwcn3XlwBnP1OpQ1/51BNawJe/212MJLPMbvJ6m8aYmhrmxVoRAb5s\n2O/e2pBb/bUsJ7OkXhxBkY2DRZcKseehbXevZFa1pMnGocxnVn1a9dfy/RozPM97\nDSzZCSFIcrYsRwz0itirK3N6zQKBgQC423Rb0iDikvKZWHNxoCTCB4QVeJ29vZzP\n2wmfFSTANS0cvYLytLPA2OxQE1aeJUpiXbM+zrs3IstBdS4kDSnt/l1UFZy15ycv\nfOWGLVTw4WtFjat1mrCdIVM2ryp1S+jJ4vtXGV3Lbl4ABcFFhGeNX4dDhKelxQkb\n/s7ECHwV4QKBgGDK6rVLx3cjva6gJJIADnlMsy1+aF7OC2rcOiv70fZwv18OMTdr\nHzh0RydA1RIinS+hMUMwHCLvlMp4AKnm3HALgrz9mq0xDjyXZ9Y3hvzCglk6lg7D\nblm9yMRQMI9qBnwuGS3RgkgyoZtSMBE1Kg+n1Bc7lL9UUcE0B/Tk7m31AoGAedXZ\n+PKjTfHAByVu4QLFd0Rw353hiT7BbZVV4A2VbUEe/IRDHCfRGVdfWN+GBTFatz4+\ndzTV218JS5kp0gUDD6LIbDTaVmGFSB0vQaH7C8/mXa8jxJ0xrj5ak/4bI7fmM7lw\nIlXLpdqWBbnFSkYb4eeGRQ/XVsBoYpUsPbPB5MECgYEAqz5DwFDUSyPheL0JdXBd\nSPqteEmO+yL5gMawpx0HD/5FEictNrvdmP43Qn3LoZ/HrRspE4Ygigk5d3c5bLgW\nLhlDFGJIKaJq5V3RWwZIJohs/hOpCdrOCY3MN1nYk1kBuDPcjothavpPkxNn0dlJ\nPzTD7Gj9aiZjryDWo6c6CWE=\n-----END PRIVATE KEY-----\n",
-  "client_email": "minimum-hexscan@minimum-hexscan.iam.gserviceaccount.com",
-  "client_id": "113777463626292406089",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/minimum-hexscan%40minimum-hexscan.iam.gserviceaccount.com"
-}
+  // Google Key
+};
